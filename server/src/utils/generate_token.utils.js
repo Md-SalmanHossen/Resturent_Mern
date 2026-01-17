ed299aa7c9e7ce732c353293890f1f75aebbbc6d
SalmanHossen <hossainsalman873@gmail.com>
2026-01-17 12:22:32 +0600

    create utils for generate token

---
+++ server/src/utils/generate_token.utils.js
@@ -0,0 +1,21 @@
+import jwt from "jsonwebtoken";
+
+const generateToken = (res, payload) => {
+  
+   if (!process.env.JWT_SECRET) {
+    throw new Error("JWT_SECRET is not defined");
+  }
+
+  const token = jwt.sign(payload, process.env.JWT_SECRET,{ expiresIn: "3d"});
+
+  res.cookie("token", token, {
+    httpOnly: true,
+    secure: process.env.NODE_ENV == "production",
+    sameSite: "strict",
+    maxAge: 3 * 24 * 60 * 60 * 1000,
+  });
+
+  return token;
+};
+
+export default generateToken;
