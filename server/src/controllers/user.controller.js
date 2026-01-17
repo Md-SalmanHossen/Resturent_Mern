decc779a3a2f13ead2872ea15b3bd3bc449edcf4
SalmanHossen <hossainsalman873@gmail.com>
2026-01-17 13:22:32 +0600

    user login controller create

--- server/src/controllers/user.controller.js
+++ server/src/controllers/user.controller.js
@@ -1,4 +1,5 @@
 import User from "./../models/user.model.js";
+import generateToken from './../utils/generate_token.utils';
 
 export const signup = async (req, res) => {
   try {
@@ -18,18 +19,17 @@ export const signup = async (req, res) => {
       });
     }
 
-    const user=await User.create({
+    const user = await User.create({
       name,
       email,
-      password
+      password,
     });
 
     res.status(201).json({
-      success:true,
-      message:'User created successfully',
-      user
+      success: true,
+      message: "User created successfully",
+      user,
     });
-    
   } catch (error) {
     res.status(500).json({
       success: false,
@@ -38,4 +38,80 @@ export const signup = async (req, res) => {
   }
 };
 
+export const userLogin = async (req, res) => {
+  try {
+    const {email,password}=req.body;
+    if(!email || !password){
+      return res.status(400).json({
+         success:false,
+         message:'All fields are require'
+      });
+    }
 
+    const user=await User.findOne({email});
+    if(!user){
+      return res.status(404).json({
+         success:false,
+         message:'user not found'
+      });
+    }
+
+    const token=generateToken(res,{id:user._id,role:user.isAdmin?'admin':'user'});
+    
+    res.status(201).json({
+      success:true,
+      message:'User login successfully',
+      token:token,
+      user:{
+         name:name,
+         email:email,
+      },
+    });
+
+  } catch (error) {
+    res.status(500).json({
+      success: false,
+      message: `Server error: ${error.message}`,
+    });
+  }
+};
+
+export const adminLogin = async (req, res) => {
+  try {
+  } catch (error) {
+    res.status(500).json({
+      success: false,
+      message: `Server error: ${error.message}`,
+    });
+  }
+};
+
+export const logout = async (req, res) => {
+  try {
+  } catch (error) {
+    res.status(500).json({
+      success: false,
+      message: `Server error: ${error.message}`,
+    });
+  }
+};
+
+export const getProfile = async (req, res) => {
+  try {
+  } catch (error) {
+    res.status(500).json({
+      success: false,
+      message: `Server error: ${error.message}`,
+    });
+  }
+};
+
+export const isAuth = async (req, res) => {
+  try {
+  } catch (error) {
+    res.status(500).json({
+      success: false,
+      message: `Server error: ${error.message}`,
+    });
+  }
+};
