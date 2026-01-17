71d1e0ad84cd691d2ede1f0b9702ca1e38caf9bf
SalmanHossen <hossainsalman873@gmail.com>
2026-01-17 12:53:59 +0600

    Create signup controller

--- server/src/controllers/user.controller.js
+++ server/src/controllers/user.controller.js
@@ -1,33 +1,41 @@
+import User from "./../models/user.model.js";
 
-import User from './../models/user.model.js';
+export const signup = async (req, res) => {
+  try {
+    const { name, email, password } = req.body;
+    if ((!name, !email, !password)) {
+      return res.status(400).json({
+        success: false,
+        message: "Please fill all the fields",
+      });
+    }
+
+    const existsUser = await User.findOne({ email }).populate("-password");
+    if (existsUser) {
+      return res.status(400).json({
+        success: false,
+        message: "User already exists",
+      });
+    }
+
+    const user=await User.create({
+      name,
+      email,
+      password
+    });
+
+    res.status(201).json({
+      success:true,
+      message:'User created successfully',
+      user
+    });
+    
+  } catch (error) {
+    res.status(500).json({
+      success: false,
+      error: error.message,
+    });
+  }
+};
 
 
-
-export const signup=async(req ,res)=>{
-   try {
-
-      const {name,email,password}=req.body;
-      if(!name ,!email,!password){
-         return res.status(400).json({
-            success:false,
-            message:'Please fill all the fields'
-         });
-      }
-
-      const existsUser=await User.findOne({email}).populate('-password');
-      if(existsUser){
-         return res.status(400).json({
-            success:false,
-            message:'User already exists',
-         });
-      }
-
-      
-
-   } catch (error) {
-      res.status(500).json({
-         success:false,
-         error:error.message
-      })
-   }
-}
